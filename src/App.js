2ab7928ae6d7c2e2d9070e984b030b63278d146c
Min Su Kim <kim97minsu@gmail.com>
2021-03-10 12:02:07 -0800

    set up files and installations

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="navigation">
+        {/* <img src={logo} className="logo" alt="Logo Image" /> */}
+        <div className="navigation-sub">
+          <a href="" className="item">
+            Projects
+          </a>
+          <a href="" className="item">
+            Articles
+          </a>
+          <a href="" className="item">
+            About
+          </a>
+        </div>
+      </div>
     </div>
   );
 }
