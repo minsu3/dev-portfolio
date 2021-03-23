fec9f25043f4652ff9c3992c7ca1e6a05ac39eb7
Min Su Kim <kim97minsu@gmail.com>
2021-03-23 14:40:23 -0700

    removed hashrouter

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "../src/styles/index.css";
-import { HashRouter } from "react-router-dom";
+import { BrowserRouter as Router } from "react-router-dom";
 import App from "./App";
 
 ReactDOM.render(
-  <HashRouter basename="/">
+  <Router>
     <App />
-  </HashRouter>,
+  </Router>,
   document.getElementById("root")
 );
